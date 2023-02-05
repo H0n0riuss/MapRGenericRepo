ae8a3d7351c3aed94b7896a793e30c0403e5e6c7
H0n0riuss <joscha.reichenberg@web.de>
2023-02-05 16:09:43 +0100

    :sparkles: not implemented ExtendedCRUDRepositorycrud

--- src/main/java/io/github/honoriuss/crud/repositories/ExtendedCRUDRepository.java
+++ src/main/java/io/github/honoriuss/crud/repositories/ExtendedCRUDRepository.java
@@ -3,6 +3,7 @@ package io.github.honoriuss.crud.repositories;
 import io.github.honoriuss.crud.entities.AEntity;
 
 import java.sql.Connection;
+import java.util.List;
 
 /**
  * @author H0n0riuss
@@ -11,4 +12,29 @@ public class ExtendedCRUDRepository<T extends AEntity> extends CRUDMapRRepositor
     public ExtendedCRUDRepository(Connection connection, String dbPath) {
         super(connection, dbPath);
     }
+
+    public List<T> createMany(List<T> newEntries) {
+        return null;
+    }
+
+    public List<T> readMany(int amount, int offset, String order) {
+        return null;
+    }
+
+    public List<T> updateMany(List<T> updatedEntries) {
+        return null;
+    }
+
+    public List<T> deleteMany(List<String> deleteKeys) {
+        return null;
+    }
+
+    public T findEntry(String contains, String column) {
+        return null;
+    }
+
+    public List<T> findMany(String contains, String column, int amount, int offset, String order) {
+        return null;
+    }
+
 }
