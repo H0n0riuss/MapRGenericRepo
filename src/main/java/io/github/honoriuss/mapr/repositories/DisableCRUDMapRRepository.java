ea120a9ad73e7f00d4ec86a6052df01b4872caf8
Joscha Reichenberg <joscha.reichenberg@hs-furtwangen.de>
2023-02-24 21:07:19 +0100

    :sparkles: disableRepository

---
+++ src/main/java/io/github/honoriuss/mapr/repositories/DisableCRUDMapRRepository.java
@@ -0,0 +1,33 @@
+package io.github.honoriuss.mapr.repositories;
+
+import io.github.honoriuss.mapr.repositories.entities.ADisableEntity;
+import org.ojai.store.Connection;
+import org.ojai.store.DocumentStore;
+
+public class DisableCRUDMapRRepository<T extends ADisableEntity> extends CRUDMapRRepository<T> {
+    public DisableCRUDMapRRepository(Connection connection, String dbPath, Class<T> tClass) {
+        super(connection, dbPath, tClass);
+    }
+
+    public boolean disableEntry(String id) {
+        return changeEntry(id, true);
+    }
+
+    public boolean enableEntry(String id) {
+        return changeEntry(id, false);
+    }
+
+    private boolean changeEntry(String id, boolean disable){
+        try (DocumentStore store = connection.getStore(dbPath)) {
+            var entity = store.findById(id).toJavaBean(tClass);
+            if (entity == null) {
+                return false;
+            }
+            entity.disabled = disable;
+            var doc = connection.newDocument(entity);
+            store.insert(id, doc);
+
+            return true;
+        }
+    }
+}
---
+++ src/main/java/io/github/honoriuss/mapr/repositories/entities/ADisableEntity.java
@@ -0,0 +1,5 @@
+package io.github.honoriuss.mapr.repositories.entities;
+
+public class ADisableEntity extends AEntity {
+    public boolean disabled = false;
+}
