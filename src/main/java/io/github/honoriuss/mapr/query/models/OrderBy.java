876d5ae3b6743c05283f08461dba1a3e9e05f566
Joscha Reichenberg <joscha.reichenberg@hs-furtwangen.de>
2023-12-08 12:33:47 +0100

    :sparkles: OrderBy :white_check_mark:

--- src/main/java/io/github/honoriuss/mapr/query/models/OrderBy.java
+++ src/main/java/io/github/honoriuss/mapr/query/models/OrderBy.java
@@ -1,10 +1,66 @@
 package io.github.honoriuss.mapr.query.models;
 
+import io.github.honoriuss.mapr.utils.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
 /**
  * @author H0n0riuss
  */
 public class OrderBy {
-    public OrderBy(String source) {
+    private static final Pattern ORDER_BY = Pattern.compile("(OrderBy|Order)");
+    private static final Pattern DIRECTION = Pattern.compile("(ASC|DESC|Asc|Desc)");
+    private final List<String> attributesToOrderBy = new ArrayList<>();
+    private boolean isAsc = false; //TODO prüfen was der Standard ist
 
+    public OrderBy(String source) {
+        Assert.notNull(source, "Source can`t be null.");
+        if (!ORDER_BY.matcher(source).find()) {
+            return;
+        }
+        extractOrderBy(source);
+    }
+
+    public Optional<List<String>> getAttributesToOrderBy() {
+        if (this.attributesToOrderBy.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(attributesToOrderBy);
+    }
+
+    public boolean isAsc() {
+        return isAsc;
+    }
+
+    private void extractOrderBy(String source) {
+        var orderBy = source
+                .split("(OrderBy|Order)")[1]
+                .split("\\(")[0]; //TODO das mit den anderen Keywörtern machen --> so kann es nur am Ende stehen!
+        orderBy = findDirection(orderBy);
+
+        var split = orderBy.split("(?=\\p{Lu})");
+
+        this.attributesToOrderBy.addAll(Arrays.asList(split));
+    }
+
+    private String findDirection(String orderBy) {
+        if (!DIRECTION.matcher(orderBy).find()) {
+            return orderBy;
+        }
+
+        var asc = "(ASC|Asc)";
+        var desc = "(DESC|Desc)";
+        if (Pattern.compile(asc).matcher(orderBy).find()) {
+            orderBy = orderBy.split(asc)[0];
+            this.isAsc = true;
+        } else if (Pattern.compile(desc).matcher(orderBy).find()) {
+            orderBy = orderBy.split(desc)[0];
+        }
+
+        return orderBy;
     }
 }
