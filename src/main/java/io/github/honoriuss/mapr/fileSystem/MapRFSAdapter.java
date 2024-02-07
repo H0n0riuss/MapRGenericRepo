53aa7856898992045fd8a826e3365c1e826eba0d
H0n0riuss <joscha.reichenberg@web.de>
2024-02-07 22:24:12 +0100

    :sparkles: MapRFSAdapter getFileStatusList recursive

--- src/main/java/io/github/honoriuss/mapr/fileSystem/MapRFSAdapter.java
+++ src/main/java/io/github/honoriuss/mapr/fileSystem/MapRFSAdapter.java
@@ -5,11 +5,13 @@ import oadd.org.apache.hadoop.fs.FileSystem;
 import oadd.org.apache.hadoop.fs.LocatedFileStatus;
 import oadd.org.apache.hadoop.fs.Path;
 import oadd.org.apache.hadoop.fs.RemoteIterator;
+import oadd.org.apache.hadoop.fs.FileStatus;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public abstract class MapRFSAdapter {
     public static InputStream readFile(String filePath) throws IOException {
@@ -26,6 +28,47 @@ public abstract class MapRFSAdapter {
             }
         }
         return files;
+    }
+
+    public static List<FileStatus> getFileStatusList(Path path) throws IOException {
+        var fileStatusList = new ArrayList<FileStatus>();
+        var fileStatuses = getFs().listStatus(path);
+
+        for (var fileStatus : fileStatuses) {
+            if (fileStatus.isDirectory()) {
+                fileStatusList.addAll(getFileStatusList(fileStatus.getPath()));
+            } else {
+                fileStatusList.add(fileStatus);
+            }
+        }
+
+        return fileStatusList;
+    }
+
+    public static List<FileStatus> getFileStatusList(Path path, List<String> ignoredRegexList) throws IOException {
+        var fileStatusList = new ArrayList<FileStatus>();
+        var fileStatuses = getFs().listStatus(path);
+
+        for (var fileStatus : fileStatuses) {
+            var ignore = false;
+            for (String ignoredRegex : ignoredRegexList) {
+                if (Pattern.matches(ignoredRegex, fileStatus.getPath().toString())) {
+                    ignore = true;
+                    break;
+                }
+            }
+            if (ignore) {
+                continue;
+            }
+
+            if (fileStatus.isDirectory()) {
+                fileStatusList.addAll(getFileStatusList(fileStatus.getPath()));
+            } else {
+                fileStatusList.add(fileStatus);
+            }
+        }
+
+        return fileStatusList;
     }
 
     public static FileSystem getFs() throws IOException {
