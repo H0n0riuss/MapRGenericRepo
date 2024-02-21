282d2c35d0d4cd8e64c8e1feea3873f77e15ac6b
H0n0riuss <joscha.reichenberg@web.de>
2024-02-21 15:25:23 +0100

    :sparkles: adding table creation tool

--- src/main/java/io/github/honoriuss/mapr/utils/MapRUtils.java
+++ src/main/java/io/github/honoriuss/mapr/utils/MapRUtils.java
@@ -1,6 +1,7 @@
 package io.github.honoriuss.mapr.utils;
 
 import com.mapr.db.MapRDB;
+import com.mapr.db.exceptions.DBException;
 import io.github.honoriuss.mapr.fileSystem.MapRFSAdapter;
 import org.apache.hadoop.fs.Path;
 
@@ -26,15 +27,33 @@ public abstract class MapRUtils {
     }
 
     /**
-     * @param tableFullPath
-     * @return
+     * Creates a mapr Database
+     * @param tablePath path to the table
+     * @param tableName Database name
+     * @return true if everything works out
+     * @throws IOException folder cant be created
+     * @throws DBException table cant be created
      */
-    public static boolean createTable(String tableFullPath) throws IOException {
+    public static boolean createTable(String tablePath, String tableName) throws IOException, DBException {
+        if (tablePath.endsWith("/")) {
+            return createTable(tablePath.concat(tableName));
+        }
+        return createTable(tablePath.concat("/").concat(tableName));
+    }
+
+    /**
+     * Creates a mapr Database
+     * example: "your/path/to/table/tableName"
+     * @param tableFullPath with tableName
+     * @return true if create directory and table creation succeeds
+     */
+    public static boolean createTable(String tableFullPath) throws IOException, DBException {
         var lasIndexOf = tableFullPath.lastIndexOf("/");
         var dir = tableFullPath.substring(0, lasIndexOf);
         createDirectoryPath(dir);
-        if(!MapRDB.tableExists(tableFullPath)){ //TODO
-
+        if (!MapRDB.tableExists(tableFullPath)) {
+            MapRDB.createTable(tableFullPath).close();
+            return true;
         }
         return false;
     }
